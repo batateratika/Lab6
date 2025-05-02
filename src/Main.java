ecf942bd99858a4fbd521596a2bdf439fc00e3c4
batateratika <batateratika@gmail.com>
2025-05-02 12:18:37 +0200

    Replaced welcome msg to loop of "Hello, World"

--- src/Main.java
+++ src/Main.java
@@ -1,10 +1,8 @@
 public class Main {
     public static void main(String[] args) {
 
-        System.out.printf("Hello and welcome!\n");
-
         for (int i = 1; i <= 5; i++) {
-            System.out.println("i = " + i);
+            System.out.println("Hello, world!");
         }
     }
 }
